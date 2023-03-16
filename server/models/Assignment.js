9824d10e068066799705399362ee0d1658745c05
Nicholas Miller <nicholasmill157@gmail.com>
2023-03-16 18:49:11 -0400

    created collaborator, assignment schema, auth and date utils

---
+++ server/models/Assignment.js
@@ -0,0 +1,35 @@
+// Imports
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+// Assignment Schema
+const assignmentSchema = new Schema({
+    taskTitle: {
+        type: String,
+        required: 'A task name needs to be assigned!',
+        minlength: 2,
+        maxlength: 50,
+        trim: true,
+    },
+    taskDesc: {
+        type: String,
+        required: 'A task needs a description!',
+        minlength: 10,
+        maxlength: 500,
+        trim: true,
+    },
+    userName: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+    assignedAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+});
+
+const Assignment = model('Assignment', assignmentSchema);
+
+module.exports = Assignment;
