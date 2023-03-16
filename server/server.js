2bb5f5ed005454d8564ad938298fa23e5fc1ae16
Ale-Miret <112821785+Ale-Miret@users.noreply.github.com>
2023-03-16 19:02:15 -0400

    added server.js and boilerplate code

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const auth = require('./middleware/auth');
+const app = express();
+dotenv.config();
+
+// Connect to MongoDB database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
+
+// Enable cross-origin resource sharing
+app.use(cors());
+
+// Apply authentication middleware to all routes except login and register
+app.use('/api', auth);
+
+// Create GraphQL server with ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ user: req.user })
+});
+server.applyMiddleware({ app, path: '/api' });
+
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
