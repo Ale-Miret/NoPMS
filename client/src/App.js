b8bee487f99db1ed88cffd1788aac19c2a962236
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-03-16 19:10:20 -0400

    added comments in App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -23,3 +23,39 @@ function App() {
 }
 
 export default App;
+
+
+// import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// // import components
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Project from "./components/Project";
+
+// // import pages
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Portfolio from "./pages/Portfolio";
+// import Resume from "./pages/Resume";
+
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <div>
+//           <Header />
+//           <Routes>
+//             <Route path="/" element={<About />} />
+//             <Route path="/portfolio" element={<Portfolio />} />
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/resume" element={<Resume />} />
+//           </Routes>
+//           <Footer />
+//         </div>
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
