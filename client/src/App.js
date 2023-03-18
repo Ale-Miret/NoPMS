ab6d741498a4be040c4ab8be77f4f6a213c73270
jon-dev092 <jp106833@yahoo.com>
2023-03-17 20:05:43 -0400

    added new dependencies and setup for pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import components
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Project from "./components/Project";
+
 
 // import pages
 import ProjectDetails from "./pages/ProjectDetails";
@@ -17,14 +15,12 @@ function App() {
     <>
       <Router>
         <div>
-          <Header />
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/project" element={<Project />} />
             <Route path="/details" element={<ProjectDetails />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
-          <Footer />
         </div>
       </Router>
     </>
