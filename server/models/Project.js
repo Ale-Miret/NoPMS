4a9eda7cb8c872aafecc9fd43cf3049c2191cbaa
jon-dev092 <jp106833@yahoo.com>
2023-03-18 14:07:36 -0400

    took out comments in project.js

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,9 +1,9 @@
 const { Schema } = require('mongoose');
 
-// import schema from User.js
+
 const collaboratorSchema = require('./Collaborator');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+
 const projectSchema = new Schema({
   projectName: {
       type: String,
@@ -13,7 +13,7 @@ const projectSchema = new Schema({
     type: String,
     required: true,
   },
-  // Link to github repository
+ 
   gitHubLink: {
     type: String,
     required: true,
