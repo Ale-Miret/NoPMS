833d16e5bfc5836806aace378524965b8b0de2e8
Ale-Miret <112821785+Ale-Miret@users.noreply.github.com>
2023-03-17 20:05:09 -0400

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import components
+// import pages
+import ProjectDetails from "./pages/ProjectDetails";
+import Contact from "./pages/Contact";
+import Project from "./pages/Project";
+import Login from "./pages/Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/details" element={<ProjectDetails />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
-
 export default App;
-
-
-// import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// // import components
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import Project from "./components/Project";
-
-// // import pages
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Portfolio from "./pages/Portfolio";
-// import Resume from "./pages/Resume";
-
-// function App() {
-//   return (
-//     <>
-//       <Router>
-//         <div>
-//           <Header />
-//           <Routes>
-//             <Route path="/" element={<About />} />
-//             <Route path="/portfolio" element={<Portfolio />} />
-//             <Route path="/contact" element={<Contact />} />
-//             <Route path="/resume" element={<Resume />} />
-//           </Routes>
-//           <Footer />
-//         </div>
-//       </Router>
-//     </>
-//   );
-// }
-
-// export default App;
