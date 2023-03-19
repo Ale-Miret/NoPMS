8586b84bc5e65063c565d012cbf261c88e9342e1
Ale-Miret <112821785+Ale-Miret@users.noreply.github.com>
2023-03-19 10:08:15 -0400

    added useLocation hook from reactrouterdom to get the current path of the page replaced isActive with the className to fix error in console

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,36 +1,70 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+
+
+
+// export default function Navigation() {
+//     return (
+//         <>
+//       <nav>
+//         <Link isActive={() => window.location.pathname === "/"} to="/">
+//           Login
+//         </Link>
+//         <br></br>
+//         <Link isActive={() => window.location.pathname === "/signup"} to="/signup">
+//           Signup
+//         </Link>
+//         <br></br>
+//         <Link
+//           isActive={() => window.location.pathname === "/projects"}
+//           to="/projects"
+//         >
+//           Projects
+//         </Link>
+//         <br></br>
+//         <Link
+//           isActive={() => window.location.pathname === "/projects/create"}
+//           to="/cprojects/create"
+//         >
+//           Create Project
+//         </Link>
+//       </nav>
+//       </>
+//     );
+//   }
+  
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-
-
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Navigation() {
-    return (
-        <>
-      <nav>
-        <Link isActive={() => window.location.pathname === "/"} to="/">
-          Login
-        </Link>
-        <br></br>
-        <Link isActive={() => window.location.pathname === "/signup"} to="/signup">
-          Signup
-        </Link>
-        <br></br>
-        <Link
-          isActive={() => window.location.pathname === "/projects"}
-          to="/projects"
-        >
-          Projects
-        </Link>
-        <br></br>
-        <Link
-          isActive={() => window.location.pathname === "/projects/create"}
-          to="/cprojects/create"
-        >
-          Create Project
-        </Link>
-      </nav>
-      </>
-    );
-  }
-  
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return path === location.pathname ? 'active' : '';
+  };
+
+  return (
+    <nav>
+      <Link className={isActive('/')} to="/">
+        Login
+      </Link>
+      <br />
+      <Link className={isActive('/signup')} to="/signup">
+        Signup
+      </Link>
+      <br />
+      <Link className={isActive('/projects')} to="/projects">
+        Projects
+      </Link>
+      <br />
+      <Link
+        className={isActive('/projects/create')}
+        to="/cprojects/create"
+      >
+        Create Project
+      </Link>
+    </nav>
+  );
+}
+
