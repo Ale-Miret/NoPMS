9dd47b538817dab1ddaf5a47a23b2ed460bbcca2
Nicholas Miller <nicholasmill157@gmail.com>
2023-03-22 03:35:52 -0400

    Able to search up users now

--- client/src/pages/ProjectDetails.js
+++ client/src/pages/ProjectDetails.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { GET_PROJECT } from "../utils/queries";
+import { Link } from "react-router-dom";
 
 const ProjectDetails = () => {
   const { projectId } = useParams();
@@ -13,12 +14,17 @@ const ProjectDetails = () => {
   if (error) return <p>Error: {error.message}</p>;
 
   const { project } = data;
+  console.log(project._id)
+
 
   return (
     <div>
       <h1>{project.projectName}</h1>
       <p>{project.description}</p>
       <p>GitHub Link: {project.gitHubLink}</p>
+      <Link to={`/project/${project._id}/collaborators`}>
+            <button>Add Collaborator</button>
+          </Link>
       <h2>Collaborators:</h2>
       <ul>
         {project.projectCollaborators.map((collaborator) => (
