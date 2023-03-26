964314895b1ab80bf6bf54bdd78966aaebb8a236
Ale-Miret <112821785+Ale-Miret@users.noreply.github.com>
2023-03-26 09:38:57 -0400

    added aditional text to InfoSection

--- client/src/components/InfoSection.js
+++ client/src/components/InfoSection.js
@@ -6,6 +6,7 @@ const InfoSection = () => {
   return (
     <Box className="info-section" py={20} px={4}>
       <Heading className="info-header" as="h2" size="2xl" mb={5}>Welcome to Our Project Management Tool</Heading>
+      <Text className="info-text" fontSize="2xl" mb={5}> No plan? No problem!</Text>
       <Text className="info-text" fontSize="2xl" mb={5}>Our tool allows you to <span style={{fontStyle: "italic"}}>easily</span> manage your projects, collaborate with team members, <span style={{fontStyle: "italic"}}>and </span>stay informed about progress!</Text>
       <Button as={Link} to="/signup" className="info-button" variant="solid" size="lg">Get Started</Button>
     </Box>
