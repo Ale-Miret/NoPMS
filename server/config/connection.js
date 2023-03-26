0b84122d1af189b499c8757bb65d5cf3508c3769
jon-dev092 <jp106833@yahoo.com>
2023-03-26 01:17:17 -0400

    deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/nopms', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/NoPMS',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
